14ed7642b4c6f18c85053b329cd0a1ce5714544c
Hunter Jansen <rawkamatic@gmail.com>
2016-08-30 09:24:00 -0400

    Add server logic for refreshing token

--- client/app/app.js
+++ client/app/app.js
@@ -10,30 +10,28 @@ var images = [];
 
 function getImages() {
   $.get("/api/images", function (data) {
-    images = data;
-    console.log(images);
-
-    if (data.length > 0) {
-      data.forEach(function (image) {
-        var img = $('<img>'); //Equivalent: $(document.createElement('img'))
-        img.attr('src', image.url);
-        img.attr('class', 'grid-item');
-        img.appendTo('#grid');
-      });
-
-      var $grid = $('#grid').packery({
-        itemSelector: '.grid-item',
-        gutter: 0
-      });
-
-      console.log($grid);
-
-      $grid.imagesLoaded().progress(function () {
-        $grid.packery();
-        console.log('face');
-      });
-    }
+    appendImages(data);
   });
+};
+
+function appendImages(images) {
+  if (images.length > 0) {
+    images.forEach(function (image) {
+      var img = $('<img>'); //Equivalent: $(document.createElement('img'))
+      img.attr('src', image.url);
+      img.attr('class', 'grid-item');
+      img.appendTo('#grid');
+    });
+
+    var $grid = $('#grid').packery({
+      itemSelector: '.grid-item',
+      gutter: 0
+    });
+
+    $grid.imagesLoaded().progress(function () {
+      $grid.packery();
+    });
+  }
 };
 
 $().ready(function () {
--- server/src/image/image.controller.js
+++ server/src/image/image.controller.js
@@ -1,4 +1,10 @@
 'use strict';
+var request = require('request');
+var fs = require('fs');
+var path = require('path');
+var pemFile = path.resolve(__dirname, '../../../mykey.pem');
+var tokenFile = path.resolve(__dirname, '../../../token');
+var jwt = require('jsonwebtoken');
 
 /**
  * GET /images
@@ -7,34 +13,56 @@
  * list of things
  *
  */
-exports.find = function (req, res) {
+exports.find = function () {
   console.log('Get');
-  return res.status(200).json(testJson)
-  // Thing.find(function(err, things) {
-  //   if (err) {
-  //     return next(err);
-  //   }
-  //   return res.status(200).json(things);
-  // });
-
+  refreshToken();
+  // return res.status(200).json(testJson)
 };
 
-var testJson =
-  [{ id: 1, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099318_1783608118591244_4070961_n.jpg?ig_cache_key=MTMyNDk1NjkwMzU5NjIzMjkyMw%3D%3D.2' }, 
-  { id: 2, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14027283_302037656830962_408979627_n.jpg?ig_cache_key=MTMyNDk4NTczNzgwODcxNTQ3Mw%3D%3D.2' }, 
-  { id: 3, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p750x750/14063676_1946131265613524_46444315_n.jpg?ig_cache_key=MTMyNDkzMDU2MjMwMTgyMjc0NQ%3D%3D.2' }, 
-  { id: 4, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14134967_110523832734202_580520169_n.jpg?ig_cache_key=MTMyNDk0MjM5MTA0NDMwMTU2OQ%3D%3D.2' }, 
-  { id: 5, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099770_1110357952363317_147541195_n.jpg?ig_cache_key=MTMyNDk0Mzc0NzY5OTkzMDc5NA%3D%3D.2' }, 
-  { id: 6, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14135019_171885673221224_534207667_n.jpg?ig_cache_key=MTMyNDkzMDMzNTY4MjY5MzkwNg%3D%3D.2' },
-  { id: 1, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099318_1783608118591244_4070961_n.jpg?ig_cache_key=MTMyNDk1NjkwMzU5NjIzMjkyMw%3D%3D.2' }, 
-  { id: 2, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14027283_302037656830962_408979627_n.jpg?ig_cache_key=MTMyNDk4NTczNzgwODcxNTQ3Mw%3D%3D.2' }, 
-  { id: 3, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p750x750/14063676_1946131265613524_46444315_n.jpg?ig_cache_key=MTMyNDkzMDU2MjMwMTgyMjc0NQ%3D%3D.2' }, 
-  { id: 4, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14134967_110523832734202_580520169_n.jpg?ig_cache_key=MTMyNDk0MjM5MTA0NDMwMTU2OQ%3D%3D.2' }, 
-  { id: 5, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099770_1110357952363317_147541195_n.jpg?ig_cache_key=MTMyNDk0Mzc0NzY5OTkzMDc5NA%3D%3D.2' }, 
-  { id: 6, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14135019_171885673221224_534207667_n.jpg?ig_cache_key=MTMyNDkzMDMzNTY4MjY5MzkwNg%3D%3D.2' },
-  { id: 1, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099318_1783608118591244_4070961_n.jpg?ig_cache_key=MTMyNDk1NjkwMzU5NjIzMjkyMw%3D%3D.2' }, 
-  { id: 2, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14027283_302037656830962_408979627_n.jpg?ig_cache_key=MTMyNDk4NTczNzgwODcxNTQ3Mw%3D%3D.2' }, 
-  { id: 3, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p750x750/14063676_1946131265613524_46444315_n.jpg?ig_cache_key=MTMyNDkzMDU2MjMwMTgyMjc0NQ%3D%3D.2' }, 
-  { id: 4, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14134967_110523832734202_580520169_n.jpg?ig_cache_key=MTMyNDk0MjM5MTA0NDMwMTU2OQ%3D%3D.2' }, 
-  { id: 5, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099770_1110357952363317_147541195_n.jpg?ig_cache_key=MTMyNDk0Mzc0NzY5OTkzMDc5NA%3D%3D.2' }, 
-  { id: 6, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14135019_171885673221224_534207667_n.jpg?ig_cache_key=MTMyNDkzMDMzNTY4MjY5MzkwNg%3D%3D.2' }];
+
+function refreshToken() {
+  var date = new Date();
+  var jwto;
+  var hour = date.getTime() + (date.getHours() * 60 * 60 * 1000);
+  var key = fs.readFileSync(pemFile);
+  var assertion = {
+    'iss': 'devugc@service.account',
+    'exp': parseInt(hour.toString().substring(0, 10)),
+    'iat': parseInt(new Date().getTime().toString().substring(0, 10)),
+  };
+
+  jwto = jwt.sign(assertion, key, { algorithm: 'RS256' });
+
+  request.post({
+    url: 'https://www.socialpatrol.net/service/oauth2/token',
+    json: {
+      'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      'assertion': jwto
+    }
+  }, function (error, response, body) {
+    if (!error) {
+      fs.writeFile(tokenFile, body.access_token);
+      console.log(body.access_token);
+    }
+  });
+}
+
+// var testJson =
+//   [{ id: 1, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099318_1783608118591244_4070961_n.jpg?ig_cache_key=MTMyNDk1NjkwMzU5NjIzMjkyMw%3D%3D.2' },
+//     { id: 2, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14027283_302037656830962_408979627_n.jpg?ig_cache_key=MTMyNDk4NTczNzgwODcxNTQ3Mw%3D%3D.2' },
+//     { id: 3, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p750x750/14063676_1946131265613524_46444315_n.jpg?ig_cache_key=MTMyNDkzMDU2MjMwMTgyMjc0NQ%3D%3D.2' },
+//     { id: 4, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14134967_110523832734202_580520169_n.jpg?ig_cache_key=MTMyNDk0MjM5MTA0NDMwMTU2OQ%3D%3D.2' },
+//     { id: 5, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099770_1110357952363317_147541195_n.jpg?ig_cache_key=MTMyNDk0Mzc0NzY5OTkzMDc5NA%3D%3D.2' },
+//     { id: 6, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14135019_171885673221224_534207667_n.jpg?ig_cache_key=MTMyNDkzMDMzNTY4MjY5MzkwNg%3D%3D.2' },
+//     { id: 1, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099318_1783608118591244_4070961_n.jpg?ig_cache_key=MTMyNDk1NjkwMzU5NjIzMjkyMw%3D%3D.2' },
+//     { id: 2, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14027283_302037656830962_408979627_n.jpg?ig_cache_key=MTMyNDk4NTczNzgwODcxNTQ3Mw%3D%3D.2' },
+//     { id: 3, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p750x750/14063676_1946131265613524_46444315_n.jpg?ig_cache_key=MTMyNDkzMDU2MjMwMTgyMjc0NQ%3D%3D.2' },
+//     { id: 4, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14134967_110523832734202_580520169_n.jpg?ig_cache_key=MTMyNDk0MjM5MTA0NDMwMTU2OQ%3D%3D.2' },
+//     { id: 5, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099770_1110357952363317_147541195_n.jpg?ig_cache_key=MTMyNDk0Mzc0NzY5OTkzMDc5NA%3D%3D.2' },
+//     { id: 6, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14135019_171885673221224_534207667_n.jpg?ig_cache_key=MTMyNDkzMDMzNTY4MjY5MzkwNg%3D%3D.2' },
+//     { id: 1, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099318_1783608118591244_4070961_n.jpg?ig_cache_key=MTMyNDk1NjkwMzU5NjIzMjkyMw%3D%3D.2' },
+//     { id: 2, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14027283_302037656830962_408979627_n.jpg?ig_cache_key=MTMyNDk4NTczNzgwODcxNTQ3Mw%3D%3D.2' },
+//     { id: 3, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p750x750/14063676_1946131265613524_46444315_n.jpg?ig_cache_key=MTMyNDkzMDU2MjMwMTgyMjc0NQ%3D%3D.2' },
+//     { id: 4, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14134967_110523832734202_580520169_n.jpg?ig_cache_key=MTMyNDk0MjM5MTA0NDMwMTU2OQ%3D%3D.2' },
+//     { id: 5, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14099770_1110357952363317_147541195_n.jpg?ig_cache_key=MTMyNDk0Mzc0NzY5OTkzMDc5NA%3D%3D.2' },
+//     { id: 6, url: 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/14135019_171885673221224_534207667_n.jpg?ig_cache_key=MTMyNDkzMDMzNTY4MjY5MzkwNg%3D%3D.2' }];
