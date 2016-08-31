ceb142c2dd3349b11ee68838d5074b9ebce85001
Hunter Jansen <rawkamatic@gmail.com>
2016-08-30 23:10:50 -0400

    Lots of styling changes
    Fix layout on grid
    add basic layout for sponsors and blurb
    add load more functionality

--- client/app/app.js
+++ client/app/app.js
@@ -6,30 +6,40 @@
 
 // })();
 
-var images = [];
+var offset = 0
 
 function getImages() {
-  $.get("/api/images", function (data) {
+  $.get("/api/images?offset=" + offset, function (data) {
     appendImages(data);
+    offset++;
   });
 };
 
 function appendImages(images) {
+  //Capture current scroll position
+  var top = $(document).scrollTop();
+
+  var $grid = $('#grid').packery({
+    itemSelector: '.grid-item',
+    gutter: 0
+  });
+
   if (images.length > 0) {
     images.forEach(function (image) {
       var img = $('<img>'); //Equivalent: $(document.createElement('img'))
       img.attr('src', image.url);
       img.attr('class', 'grid-item');
-      img.appendTo('#grid');
+      $grid.append(img)
+        // add and lay out newly appended items
+        .packery('appended', img);
+      // img.appendTo('#grid');
     });
 
-    var $grid = $('#grid').packery({
-      itemSelector: '.grid-item',
-      gutter: 0
-    });
-
+    //When an image loads, get packery to relayout
     $grid.imagesLoaded().progress(function () {
       $grid.packery();
+      //Make sure everything doesn't scroll
+      $(document).scrollTop(top);
     });
   }
 };
