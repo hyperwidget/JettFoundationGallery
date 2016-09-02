e1a2d4b4fc74a53b6091d61689eaa4bb8ef1a164
Hunter Jansen <rawkamatic@gmail.com>
2016-09-01 20:09:42 -0400

    try changing workflow to just refresh token before api request

--- server/src/image/image.controller.js
+++ server/src/image/image.controller.js
@@ -18,32 +18,52 @@ var tries = 0;
 function queryServer(req, res) {
   var offset = req.offset;
   var limit = 20;
+  var date = new Date();
+  var jwto;
+  var hour = date.getTime() + (date.getHours() * 60 * 60 * 1000);
+  var key = fs.readFileSync(pemFile);
+  var assertion = {
+    'iss': 'devugc@service.account',
+    'exp': parseInt(hour.toString().substring(0, 10)),
+    'iat': parseInt(new Date().getTime().toString().substring(0, 10)),
+  };
+
+  jwto = jwt.sign(assertion, key, { algorithm: 'RS256' });
+
   request.post({
-    url: 'https://www.socialpatrol.net/api/external/jettfoundation',
-    headers: 
-      {
-        'Authorization': 'Bearer ' + fs.readFileSync(tokenFile).toString()
-      }
-    ,
-    json: { streams: [2419] }, offset: offset * limit, limit: limit
-  },
-    function (error, response, body) {
-      if (!error) {
-        if (body.messageType !== 'error') {
-          return res.status(200).json(body);
-        } else if (tries >= 3) {
-          return res.status(200).json({ errorMessage: 'Issues communicating with the server, please try again later' });
-        } else {
-          tries++;
-          console.log('Trying again; try number ' + tries );
-          refreshToken(req, res, function(req, res){
-            queryServer(req, res);
-          })
+    url: 'https://www.socialpatrol.net/service/oauth2/token',
+    json: {
+      'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      'assertion': jwto
+    }
+  }, function (error, response, body) {
+    if (!error) {
+      console.log('token success');
+      fs.writeFile(tokenFile, body.access_token);
+      request.post({
+        url: 'https://www.socialpatrol.net/api/external/jettfoundation',
+        headers:
+        {
+          'Authorization': 'Bearer ' + body.access_token
         }
-      } else {
-        console.log('ERROR');
-      }
-    });
+        ,
+        json: { streams: [2419] }, offset: offset * limit, limit: limit
+      },
+        function (error, response, body) {
+          if (!error) {
+            if (body.messageType !== 'error') {
+              return res.status(200).json(body);
+            } else {
+              return res.status(200).json({ errorMessage: 'Issues communicating with the server, please try again later' });
+            }
+          } else {
+            console.log('ERROR');
+          }
+        });
+    }
+  });
+
+
 }
 exports.find = queryServer;
 
@@ -70,7 +90,7 @@ function refreshToken(req, res, callback) {
     if (!error) {
       console.log('token success');
       fs.writeFile(tokenFile, body.access_token);
-      callback(req,res);
+      callback(req, res);
     }
   });
 }
