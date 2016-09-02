f581084b02f7c5d1d3936d2734c1ab0cf1c9e4b6
Hunter Jansen <rawkamatic@gmail.com>
2016-09-01 22:35:49 -0400

    Add an overlay with avatar and name
    Make mobile version better

--- client/app/app.js
+++ client/app/app.js
@@ -18,13 +18,33 @@ function appendImages(images) {
 
   if (images.length > 0) {
     images.forEach(function (image) {
+      var div = $('<div>');
+      div.attr('class', 'grid-item');
       var img = $('<img>');
       var pictures = JSON.parse(decodeURIComponent(image.entry_data)).pictures;
       img.attr('src', pictures[0]);
-      img.attr('class', 'grid-item');
-      $grid.append(img)
+      var overlay = $('<div>');
+      overlay.attr('class', 'overlay');
+      var avatar = $('<img>');
+      avatar.attr('src', image.author_image_url);
+      avatar.attr('class', 'avatar');
+      var authorLink = $('<a></a>');
+      authorLink.attr('href', image.author_url);      
+      authorLink.attr('target', '_blank');      
+      authorLink.append(avatar);
+      try{
+        authorLink.append(decodeURIComponent((image.author_name + '').replace(/\+/g, '%20')));
+      } catch(e){
+        console.log('Could not translate: ' + image.author_name); 
+      }
+      
+      overlay.append(authorLink);
+
+      div.append(img);
+      div.append(overlay);
+      $grid.append(div)
         // add and lay out newly appended items
-        .packery('appended', img);
+        .packery('appended', div);
       // img.appendTo('#grid');
     });
 
@@ -36,6 +56,13 @@ function appendImages(images) {
     });
   }
 };
+
+$(window).resize(function(){
+   $grid = $('#grid').packery({
+    itemSelector: '.grid-item',
+    gutter: 0
+  });
+});
 
 $().ready(function () {
   getImages();
