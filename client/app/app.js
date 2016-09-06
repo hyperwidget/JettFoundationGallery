d5573784ef872ee464b9962d7aba02a793456310
Hunter Jansen <rawkamatic@gmail.com>
2016-09-06 14:41:19 -0400

    fixes crash due to no pictures

--- client/app/app.js
+++ client/app/app.js
@@ -18,34 +18,36 @@ function appendImages(images) {
 
   if (images.length > 0) {
     images.forEach(function (image) {
-      var div = $('<div>');
-      div.attr('class', 'grid-item');
-      var img = $('<img>');
-      var pictures = JSON.parse(decodeURIComponent(image.entry_data)).pictures;
-      img.attr('src', pictures[0]);
-      var overlay = $('<div>');
-      overlay.attr('class', 'overlay');
-      var avatar = $('<img>');
-      avatar.attr('src', image.author_image_url);
-      avatar.attr('class', 'avatar');
-      var authorLink = $('<a></a>');
-      authorLink.attr('href', image.author_url);      
-      authorLink.attr('target', '_blank');      
-      authorLink.append(avatar);
-      try{
-        authorLink.append(decodeURIComponent((image.author_name + '').replace(/\+/g, '%20')));
-      } catch(e){
-        console.log('Could not translate: ' + image.author_name); 
-      }
-      
-      overlay.append(authorLink);
+      if (JSON.parse(decodeURIComponent(image.entry_data)).pictures !== undefined) {
+        var div = $('<div>');
+        div.attr('class', 'grid-item');
+        var img = $('<img>');
+        var pictures = JSON.parse(decodeURIComponent(image.entry_data)).pictures;
+        img.attr('src', pictures[0]);
+        var overlay = $('<div>');
+        overlay.attr('class', 'overlay');
+        var avatar = $('<img>');
+        avatar.attr('src', image.author_image_url);
+        avatar.attr('class', 'avatar');
+        var authorLink = $('<a></a>');
+        authorLink.attr('href', image.author_url);
+        authorLink.attr('target', '_blank');
+        authorLink.append(avatar);
+        try {
+          authorLink.append(decodeURIComponent((image.author_name + '').replace(/\+/g, '%20')));
+        } catch (e) {
+          console.log('Could not translate: ' + image.author_name);
+        }
 
-      div.append(img);
-      div.append(overlay);
-      $grid.append(div)
-        // add and lay out newly appended items
-        .packery('appended', div);
-      // img.appendTo('#grid');
+        overlay.append(authorLink);
+
+        div.append(img);
+        div.append(overlay);
+        $grid.append(div)
+          // add and lay out newly appended items
+          .packery('appended', div);
+        // img.appendTo('#grid');
+      }
     });
 
     //When an image loads, get packery to relayout
@@ -57,8 +59,8 @@ function appendImages(images) {
   }
 };
 
-$(window).resize(function(){
-   $grid = $('#grid').packery({
+$(window).resize(function () {
+  $grid = $('#grid').packery({
     itemSelector: '.grid-item',
     gutter: 0
   });
